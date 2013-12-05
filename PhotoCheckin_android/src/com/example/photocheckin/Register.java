4dce2d895fcbf1440fc9e0e0889cd49343b5639a
aeeb <sheanghyung@hotmail.com>
2013-12-05 22:25:20 +0700

    Update Register page can be validation checking

--- PhotoCheckin_android/src/com/example/photocheckin/Register.java
+++ PhotoCheckin_android/src/com/example/photocheckin/Register.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -25,8 +26,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Register extends Activity {
+public class Register extends Activity implements View.OnClickListener{
 
 	String NAME;
 	String USERNAME;
@@ -35,6 +38,16 @@ public class Register extends Activity {
 	private EditText username;
 	private EditText pass;
 	private EditText email;
+	private EditText confirmpass;
+	
+	String strName;
+	String strUsername;
+	String strPass;
+	String strConfirmPass;
+	String strEmail;
+	
+	//Set E-mail syntax  
+	private String checkE = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +59,22 @@ public class Register extends Activity {
 		username = (EditText) findViewById(R.id.username_texf);
 		pass = (EditText) findViewById(R.id.password_texf);
 		email = (EditText) findViewById(R.id.email_texf);
+		confirmpass = (EditText) findViewById(R.id.confirm_password_texf);
 
-		// submit
-		Button submit = (Button) findViewById(R.id.register_btn);
-		submit.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				new register().execute();
-			}
-		});
+		
+//		// submit
+//		Button submit = (Button) findViewById(R.id.register_btn);
+//		submit.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				new register().execute();
+//			}
+//		});
+		
+		
+		// call btn login into onClick()
+		Button call_regis = (Button) findViewById(R.id.register_btn);
+		call_regis.setOnClickListener(this);
 
 	}
 
@@ -135,7 +155,7 @@ public class Register extends Activity {
 				public void run() {
 
 					// name.setText(response);
-					Toast.makeText(Register.this, "register compleat",
+					Toast.makeText(Register.this, "register complete",
 							Toast.LENGTH_SHORT).show();
 				}
 			});
@@ -145,9 +165,127 @@ public class Register extends Activity {
 	}
 
 	//check validate
+	//Check validate of Name
+	public boolean btnValidateName(View v){
+		boolean value = true;
+			try{
+				//Get value converted to a string
+				strName = name.getText().toString().trim();
+					//Empty value checking 
+			        if(strName.isEmpty()){
+			        	Toast.makeText(v.getContext(),"Your Name must not empty", Toast.LENGTH_SHORT).show();
+			        	value = false;
+			        }
+		        }
+			
+	     	catch (NullPointerException ex) {
+	     		ex.printStackTrace();	
+	        }
+		return value;
+	
+	}
 	
 	
+	//Check validate of UserName
+	public boolean btnValidateUserName(View v){
+		boolean value = true;
+			try{
+				//Get value converted to a string
+				strUsername = username.getText().toString().trim();
+					//Empty value checking
+			        if(strUsername.isEmpty()){
+			        	Toast.makeText(v.getContext(),"Your Username must not empty", Toast.LENGTH_SHORT).show();
+			        	value = false;
+			        }
+		        }
+			
+	     	catch (NullPointerException ex) {
+	     		ex.printStackTrace();	
+	        }
+		return value;
 	
+	}
+
+	//Check validate of Password and Confirm Password
+	public boolean btnValidatePassAndConfirmPass(View v){
+		boolean value = true;
+			try{
+				//Get value converted to a string
+				strPass = pass.getText().toString().trim();
+				strConfirmPass = confirmpass.getText().toString().trim();
+					
+
+			
+			    	//Check that Password And Confirm password is equal or not
+			        Matcher matcherObj = Pattern.compile(strPass).matcher(strConfirmPass);
+			        
+			        //Empty value checking
+			        if(strPass.isEmpty()){
+			        	Toast.makeText(v.getContext(), "Password must not empty", Toast.LENGTH_SHORT).show();
+			        	value = false;
+			        }else if(strConfirmPass.isEmpty()){
+			        	Toast.makeText(v.getContext(), "ConfirmPassword must not empty", Toast.LENGTH_SHORT).show();
+			        	value = false;
+			        }else if(!matcherObj.matches()){
+			            Toast.makeText(v.getContext(), "Password is InValid", Toast.LENGTH_SHORT).show();
+			            value = false;
+			        }
+		        }
+			
+	     	catch (NullPointerException ex) {
+	     		ex.printStackTrace();	
+	        }
+		return value;
+	
+	}
+	
+	//Check validate of Email
+	public boolean btnValidateEmail(View v){
+		boolean value = true;
+			try{
+				//Get value converted to a string
+				strEmail = email.getText().toString().trim();
+				
+		    	//เช็คว่า syntax ของ E-mail กับ  E-mail ที่ User กรอกมา นั้นตรงกันหรือไม่ เก็บค่าไว้ใน matcherObj>>>True or False
+		        Matcher matcherObj = Pattern.compile(checkE).matcher(strEmail);
+		        
+		        //เช็คว่าเป็นค่าว่างหรือเปล่า?
+		        if(strEmail.isEmpty()){
+		        	Toast.makeText(v.getContext(), "Email must not empty", Toast.LENGTH_SHORT).show();
+		        	value = false;
+		        }if(!matcherObj.matches()){
+		 	        Toast.makeText(v.getContext(), "Email "+strEmail+" is Invalid", Toast.LENGTH_SHORT).show();
+		 	        value = false;
+		        }
+		        
+			}
+			
+	     	catch (NullPointerException ex) {
+	     		ex.printStackTrace();	
+	        }
+		return value;
+	
+	}
+	
+	
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.register_btn:
+			if(btnValidateName(v) && btnValidateUserName(v) && btnValidatePassAndConfirmPass(v) && btnValidateEmail(v)){
+				new register().execute();
+				Intent call_index_wallpage = new Intent(this, LoginForm.class);
+				startActivity(call_index_wallpage);
+			}
+			break;
+//		case R.id.imgbtn_register:
+//			Intent call_registerbtn = new Intent(this, Register.class);
+//			startActivity(call_registerbtn);
+//			break;
+
+		}
+
+	}
 	
 	
 }
