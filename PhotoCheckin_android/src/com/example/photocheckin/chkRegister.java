9fb6290673fa87eeba827463396b021a78a89208
combatsoul <t.keawdonhnod@gmail.com>
2013-12-28 10:30:00 +0700

    chkRegister
    chkRegister modifly

--- PhotoCheckin_android/src/com/example/photocheckin/chkRegister.java
+++ PhotoCheckin_android/src/com/example/photocheckin/chkRegister.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
                 @Override
                 protected void onCreate(Bundle savedInstanceState) {
                         super.onCreate(savedInstanceState);
+                        requestWindowFeature(Window.FEATURE_NO_TITLE);
                         setContentView(R.layout.result_register);
                         
                         //splash windows
